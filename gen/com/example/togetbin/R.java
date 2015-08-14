7d01f2ac478becc898254ba4fc6a7eb422f7ecb3
Yuan-Chang <k21asdd@hotmail.com>
2015-08-14 17:11:33 +0800

    Auto gen

--- gen/com/example/togetbin/R.java
+++ gen/com/example/togetbin/R.java
@@ -33,17 +33,20 @@ public final class R {
         public static final int sample_7=0x7f020008;
     }
     public static final class id {
-        public static final int BingoView=0x7f080003;
-        public static final int CurrentNumber=0x7f080002;
-        public static final int GameStart=0x7f080004;
-        public static final int action_settings=0x7f080006;
-        public static final int debug=0x7f080005;
-        public static final int exitBingo=0x7f080001;
-        public static final int startBingo=0x7f080000;
+        public static final int BingoView=0x7f080004;
+        public static final int CurrentNumber=0x7f080003;
+        public static final int GameStart=0x7f080005;
+        public static final int action_settings=0x7f080008;
+        public static final int debug=0x7f080006;
+        public static final int exitBingo=0x7f080002;
+        public static final int mutiBingo=0x7f080001;
+        public static final int room=0x7f080007;
+        public static final int singleBingo=0x7f080000;
     }
     public static final class layout {
         public static final int bingo_main=0x7f030000;
         public static final int game_view=0x7f030001;
+        public static final int root_list=0x7f030002;
     }
     public static final class menu {
         public static final int bingo_main=0x7f070000;
