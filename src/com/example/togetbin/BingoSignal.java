1326966cad088f4f4e496b8ffe2285b022889186
Yuan-Chang <k21asdd@hotmail.com>
2015-09-04 17:08:09 +0800

    No time to commit = =

---
+++ src/com/example/togetbin/BingoSignal.java
@@ -0,0 +1,11 @@
+package com.example.togetbin;
+
+public class BingoSignal {
+	public static final int QUERY = 0;
+	public static final int CREATE = 1;
+	public static final int CONNECT = 2;
+	public static final int STEP = 3;
+	public static final int TEARDOWN = 4;
+	public static final int CHAT = 5;
+	public static final int GAME_READY = 6;
+}
--- src/com/example/togetbin/RoomList.java
+++ src/com/example/togetbin/RoomList.java
@@ -1,7 +1,23 @@
 package com.example.togetbin;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.lang.ref.WeakReference;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -11,16 +27,36 @@ import android.widget.Toast;
 
 public class RoomList extends Activity{
 	private ListView Rlist;
-	private String[] list = {"¹]µ§","­ì¤lµ§","¿ûµ§","¤òµ§","±m¦âµ§"};
+	private ArrayList<String> ll;
+	private String[] list = {"Hello"};
 	private ArrayAdapter<String> listAdapter;
+	private Messenger mMessanger;
+	
+	private class ListHandler extends Handler{
+		WeakReference<RoomList> mAct;
+		public ListHandler(RoomList mm){
+			// TODO Auto-generated constructor stub
+			mAct = new WeakReference<RoomList>(mm);
+		}
+		@Override
+		public void handleMessage(Message msg){
+			RoomList act = mAct.get();
+			if(act == null)return;
+			listAdapter = new ArrayAdapter<String>(RoomList.this, android.R.layout.simple_list_item_1, list);
+			listAdapter.notifyDataSetChanged();
+			Log.d("Toget", "Msg done");
+		}
+	};
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.root_list);
+		ll = new ArrayList<String>();
 		Rlist = (ListView)findViewById(R.id.room);
 		listAdapter = new ArrayAdapter<String>(RoomList.this, android.R.layout.simple_list_item_1, list);
 		Rlist.setAdapter(listAdapter);
+		mMessanger = new Messenger(new ListHandler(this));
 		Rlist.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
@@ -34,5 +70,47 @@ public class RoomList extends Activity{
 				}
 			}
 		});
+		
+		new Thread(){
+			public void run() {
+				try {
+					Socket s = new Socket("163.20.34.159", 5566);
+					BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+					PrintWriter out = new PrintWriter(s.getOutputStream());
+					this.sleep(500);
+					out.println(BingoSignal.QUERY);
+					out.flush();
+					if(Integer.valueOf(in.readLine()) == BingoSignal.QUERY){
+						String data = in.readLine();
+						while(data.compareTo("Q_DONE") != 0){
+							ll.add(data);
+							data = in.readLine();
+						}
+						list = new String[ll.size()];
+						list = ll.toArray(list);
+						for(String ss : list){
+							Log.d("Toget", ss + "haha");
+						}
+						Message msg = new Message();
+						msg.what = 123;
+						try {
+							mMessanger.send(msg);
+						} catch (RemoteException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				} catch (UnknownHostException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			};
+		}.start();
 	}
 }
